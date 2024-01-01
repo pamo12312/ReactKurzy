0b9709d3a24645994ff5457c7be47330c7797617
Patrik <patri.moravec@email.cz>
2024-01-01 14:17:59 +0100

    navbar hover

--- Reactkurzy/src/components/NavBar.jsx
+++ Reactkurzy/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 
 export function NavBar() {
@@ -6,9 +6,13 @@ export function NavBar() {
         <div>
             <div className="navbar">
                 <img src="./src/assets/cap.png" alt="React Image" className="imgcap"/>
-                <div><b>Courses</b></div>
-                <div>Speakers</div>
-                <div>Locations</div>
+                <ul>
+                    <li><b>Courses</b></li>
+                    <li>Speakers</li>
+                    <li>Location</li>
+
+                </ul>
+
 
             </div>
             <hr/>
